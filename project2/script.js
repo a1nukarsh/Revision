1772230e0f8bda50d547dc1b0c6700f70ae3bff5
Anukarsh <anukarshr@gmail.com>
2023-01-09 20:59:50 +0530

    update

--- project2/script.js
+++ project2/script.js
@@ -1,3 +1,12 @@
 
 let taskbar=document.getElementsByClassName("taskbar")[0]
 let startmenu=document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    if(startmenu.style.bottom == "54px"){
+        startmenu.style.bottom = "-654px"
+    }
+    else{
+        startmenu.style.bottom = "54px"
+    }
+})
